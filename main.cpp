c0e8fff7f71a7215bc74f853277ce08d93c54ac8
Charles Hayes <charles.hayes@cfhayes.com>
2022-10-24 16:50:24 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::string;
+using std::vector;
+
+enum class State {kEmpty, kObstacle, kFinish};
+
+vector<State> ParseLine(string line) {
+    istringstream sline(line);
+    int n;
+    char c;
+    vector<State> row;
+    while (sline >> n >> c && c == ',') {
+      if (n == 0) {
+        row.push_back(State::kEmpty);
+      } else {
+        row.push_back(State::kObstacle);
+      }
+    }
+    return row;
+}
+
+vector<vector<State>> ReadBoardFile(string path) {
+  ifstream myfile (path);
+  vector<vector<State>> board{};
+  if (myfile) {
+    string line;
+    while (getline(myfile, line)) {
+      vector<State> row = ParseLine(line);
+      board.push_back(row);
+    }
+  }
+  return board;
+}
+
+string CellString(State cell) {
+  switch(cell) {
+    case State::kObstacle: return   "🚧   ";
+    case State::kFinish:   return   "🏁   ";
+    default: return                 "🟢   "; 
+  }
+}
+
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+vector<vector<State>> Search(vector<vector<State>> grid, int start[2], int goal[2]) {
+  cout << "No path found" << endl;
+  return vector<vector<State>>{};
+}
+
+void PrintBoard(const vector<vector<State>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      cout << CellString(board[i][j]);
+    }
+    cout << "\n";
+  }
+}
+
+#include "test.cpp"
+
+int main() {
+  auto board = ReadBoardFile("1.board");
+  int start[2]{0,0};
+  int goal[2]{4,5};
+  auto solution = Search(board, start, goal);
+  PrintBoard(board);
+
+  TestHeuristic();
+}
+
